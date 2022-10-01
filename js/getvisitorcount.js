e7611be5fdad383236978991d1129e008252dd45
roshan <shan181987@gmail.com>
2022-10-01 19:18:42 +0200

    git the js/getvisitorcount.js

--- js/getvisitorcount.js
+++ js/getvisitorcount.js
@@ -1,8 +1,7 @@
-<script>
+
 	fetch('https://h2w85xcmm0.execute-api.eu-central-1.amazonaws.com/Prod/add')
 		.then(() => fetch('https://h2w85xcmm0.execute-api.eu-central-1.amazonaws.com/Prod/count') )
 		.then(response => response.json())
 		.then((data) => {
 			document.getElementById('visitorCount').innerText = data.count
 		})
-</script>
